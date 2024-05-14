51970ff5feb7fc3d320b67428702c793f1f1549e
Rafael <rafael06092001@gmail.com>
2024-05-14 18:32:43 -0300

    Basics - Array

---
+++ 02-basics/myArray.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHero = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHero.push('Spider-Man');
+heroPower.push(2);
+allUsers.push({ name: 'Rafael', isActive: true });
--- 02-basics/myArray.ts
+++ 02-basics/myArray.ts
@@ -1,5 +1,22 @@
 const superHero: string[] = []
-const heroPower: number[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+   name: string
+   isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+   [255, 255, 255],
+   []
+]
 
 superHero.push('Spider-Man')
 heroPower.push(2)
+
+allUsers.push({name: 'Rafael', isActive: true})
+
+export {}
