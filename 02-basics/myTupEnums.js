b4ad76fba90834929e3163081ac211a91ece5d8c
Rafael <rafael06092001@gmail.com>
2024-05-14 23:51:55 -0300

    Basics - Tuples

---
+++ 02-basics/myTupEnums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = ['rf', 1];
+var tUser;
+tUser = ['rf', 22, true];
+var rgb = [255, 132, 12];
+var newUser = [123, 'rafael@123.com'];
+newUser[1] = 'rafa@321.br';
---
+++ 02-basics/myTupEnums.ts
@@ -0,0 +1,15 @@
+const user: (string | number)[] = ['rf', 1]
+let tUser: [string, number, boolean]
+
+tUser = ['rf', 22, true]
+
+let rgb: [number, number, number] = [255, 132, 12]
+
+type User = [number, string]
+
+const newUser: User = [123, 'rafael@123.com']
+
+newUser[1] = 'rafa@321.br'
+// newUser.push(true)
+
+export{}
