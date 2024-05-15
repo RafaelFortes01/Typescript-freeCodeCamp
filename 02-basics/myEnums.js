678859e8a17b7017f0abcf209a707ae8d27e174e
Rafael <rafael06092001@gmail.com>
2024-05-15 00:04:33 -0300

    Basics - Enums

---
+++ 02-basics/myEnums.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice[SeatChoice["AISLE"] = 10] = "AISLE";
+    SeatChoice[SeatChoice["MIDDLE"] = 11] = "MIDDLE";
+    SeatChoice[SeatChoice["WINDOW"] = 12] = "WINDOW";
+    SeatChoice[SeatChoice["FOURTH"] = 13] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var hcSeat = SeatChoice.AISLE;
---
+++ 02-basics/myEnums.ts
@@ -0,0 +1,10 @@
+enum SeatChoice {
+   AISLE = 'aisle',
+   MIDDLE = 3,
+   WINDOW,
+   FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
+
+export{}
